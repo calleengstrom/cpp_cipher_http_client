745e2968fd1b8696d41aea0eaec516c86c1e9fc5
Calle <calle.eng@live.se>
2026-04-16 13:03:29 +0200

    completed code, runs as intended

--- src/main.cpp
+++ src/main.cpp
@@ -23,24 +23,82 @@
 #include <vector>
 
 #ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    #pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 #else // POSIX
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #endif
 
 #include "http/Request.h"
 #include "http/Response.h"
 #include "json/JsonParser.h"
 
-std::optional<std::string> sendRequest(const std::string& host,
-                                       int                port,
-                                       const std::string& path)
+std::optional<std::string> sendRequest(const std::string &host,
+                                       int port,
+                                       const std::string &path)
 {
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET)
+    {
+        std::cout << "Failed to setup socket" << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return std::nullopt;
+    }
+
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8080);
+    InetPtonA(AF_INET, "172.20.203.149", &server.sin_addr.S_un.S_addr);
+
+    if (connect(sock, (SOCKADDR *)&server, sizeof(server)) == SOCKET_ERROR)
+    {
+        std::cout << "FAiled to connect " << WSAGetLastError() << std::endl;
+        closesocket(sock);
+        WSACleanup;
+        return std::nullopt;
+    }
+
+    // const std::string request = "GET /test HTTP/1.1\r\nHost: 172.20.203.149\r\nConnection: close\r\n\r\n";
+    Request request(host, port, path);
+    std::string raw_request = request.build();
+
+    if (send(sock, raw_request.c_str(), (int)raw_request.length(), 0) == SOCKET_ERROR)
+    {
+        std::cout << "Failed to send request" << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return nullptr;
+    }
+
+    char buffer[2048];
+
+    std::string raw_response;
+
+    while (1)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytesReceived > 0)
+        {
+            raw_response.append(buffer, bytesReceived);
+        }
+        else if (bytesReceived == 0)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Failed to receive response " << WSAGetLastError() << std::endl;
+            break;
+        }
+    }
+    Response response = parseResponse(raw_response);
+
     /**
      * ----------------------------------------------------------
      * STEP 1 - Create a TCP Socket
@@ -58,7 +116,7 @@ std::optional<std::string> sendRequest(const std::string& host,
      * - htons()        converts the port to network byte order
      * ----------------------------------------------------------
      */
-    
+
     /**
      * ----------------------------------------------------------
      * STEP 3 - Connect to the server
@@ -76,7 +134,7 @@ std::optional<std::string> sendRequest(const std::string& host,
      * ----------------------------------------------------------
      */
 
-    /** 
+    /**
      * ----------------------------------------------------------
      * STEP 5 - Read the response
      * recv() reads the bytes from the TCP stream into a buffer.
@@ -92,29 +150,32 @@ std::optional<std::string> sendRequest(const std::string& host,
      * Always clean up the - remember to close the socket!
      */
 
-    std::cout << "Request sent to: " << host << ":" << port << path << std::endl;
-    return std::nullopt;
+    closesocket;
+    //std::cout << "Request sent to: " << host << ":" << port << path << std::endl;
+    return response.body;
 }
 
-int main() {
-    const std::string HOST = "192.168.0.106";
-    const int         PORT = 8080;
+int main()
+{
+    const std::string HOST = "172.20.203.149";
+    const int PORT = 8080;
 
-    // Initialise Winsock (Windows only — must be done before any socket call).
-    #ifdef _WIN32
+// Initialise Winsock (Windows only — must be done before any socket call).
+#ifdef _WIN32
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
         std::cerr << "[error] WSAStartup failed\n";
         return 1;
     }
-    #endif
+#endif
 
     /**
      * ----------------------------------------------------------
      * STEP 1 - Implement the sendRequest() function
      * Start off by implementing the sendRequest() function and
      * then try it by using "/clear-text" endpoint.
-     * 
+     *
      * Parse the response and then print out the result!
      * ----------------------------------------------------------
      */
@@ -125,10 +186,10 @@ int main() {
      * Start trying to decrypt the ciphertext by sending requests
      * the the endpoints down below and decrypt them using the
      * KEY and CIPHERTEXT in the payload.
-     * 
+     *
      * You can expect a payload in JSON format like this:
      * { "key": "...", "cipherText": "..." }
-     * 
+     *
      * Suggested workflow:
      *      1. Send a GET request to the related endpoint
      *      2. Parse the Response to get the body
@@ -152,15 +213,19 @@ int main() {
         "/playfair-cipher",
     };
 
-    for (const auto& path : endpoints) {
-        /**
-         * CODE
-         */
+    for (const auto &path : endpoints)
+    {
+        // std::string p = "/ceasar-cipher";
+        auto r_response = sendRequest(HOST, PORT, path);
 
-        sendRequest(HOST, PORT, path);
+        if (r_response.has_value())
+        {
+            std::cout << r_response.value() << std::endl;
+        }
     }
-    #ifdef _WIN32
-    WSACleanup();
-    #endif
-    return 0;
+
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 0;
 }
